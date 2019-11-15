df88026f2543cde948422aab7db5152f6912b7d8
Iben A <cph-ia62@cphbusiness.dk>
2019-11-15 12:12:40 +0100

    url update

--- src/settings.js
+++ src/settings.js
@@ -1,3 +1,3 @@
-export const peoplelist = "https://kodebanditterne.dk/CA3-Backend-2.0/api/swapi/peoplelist";
-export const planetlist = "https://kodebanditterne.dk/CA3-Backend-2.0/api/swapi/planetlist";
-export const filmlist = "https://kodebanditterne.dk/CA3-Backend-2.0/api/swapi/filmlist";
+export const peoplelist = "https://ibena.dk/ibena.dk-CA3-Backend-2.0/api/swapi/peoplelist";
+export const planetlist = "https://ibena.dk/ibena.dk-CA3-Backend-2.0/api/swapi/planetlist";
+export const filmlist = "https://ibena.dk/ibena.dk-CA3-Backend-2.0/api/swapi/filmlist";
